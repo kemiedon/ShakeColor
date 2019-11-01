d3b8057cf93354154a5d82f3ab85240c09c76fba
Kemie <kemiedon@gmail.com>
2019-11-01 16:46:13 +0800

    1.0.2 搖晃切換圖片版本

--- app/src/main/java/com/example/shakecolor/MainActivity.java
+++ app/src/main/java/com/example/shakecolor/MainActivity.java
@@ -23,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         square = findViewById(R.id.square);
         btn_change = findViewById(R.id.btnChange);
-        Toast.makeText(this,"app正在執行中",Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(this, ShakeService.class);
 
         //Start Service
